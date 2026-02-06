b0a3c8cef17ceeaff5f92146fddabb818e2ed178
Рафкат <rafkat.khananov@mail.ru>
2026-02-06 21:22:45 +0500

    add task 11

---
+++ task_11.py
@@ -0,0 +1,20 @@
+d = {}
+
+password = input()
+N = int(input())
+
+for i in range(N):
+  key, value = input().split()
+  d[key] = value
+
+min_length = len(password)
+has_digits = any(char.isdigit() for char in password)
+has_upper_letter = any(char.isupper() for char in password)
+
+print(d)
+
+if (len(password) >= int(d["мин_длина"])
+    and has_digits and has_upper_letter):
+    print("Да")
+else:
+    print("Нет")
