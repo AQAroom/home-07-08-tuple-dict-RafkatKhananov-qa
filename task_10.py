dd1f48a336eacafbea2464fcf8cb670a975051ce
Рафкат <rafkat.khananov@mail.ru>
2026-02-06 13:07:00 +0500

    changes in task 10

--- task_10.py
+++ task_10.py
@@ -1,18 +1,14 @@
-d_1 = {}
-d_2 = {}
+d = {}
 
 N = int(input())
 for i in range(N):
     key, value = input().split()
-    d_1[key] = value
+    d[key] = value
 
 M = int(input())
 for i in range(M):
     key, value = input().split()
-    d_2[key] = value
+    d[key] = value
 
-for key in sorted(d_1):
-    print(f"{key}: {d_1[key]}")
-
-for key in sorted(d_2):
-    print(f"{key}: {d_2[key]}")
+for key in sorted(d):
+    print(f"{key}: {d[key]}")
