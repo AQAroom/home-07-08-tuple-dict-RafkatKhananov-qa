f0986041f717461daa694a948f528a5b808cbb0e
Рафкат <rafkat.khananov@mail.ru>
2026-02-06 12:29:01 +0500

    refactor task 9

--- task_09.py
+++ task_09.py
@@ -3,10 +3,10 @@ d = {}
 text = input()
 
 for i in text:
-  if "a" <= i <= "z":
-    if i in d:
-      d[i] += 1
-    else:
-      d[i] = 1
+    if "a" <= i <= "z":
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
 
 print(d)
